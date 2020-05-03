533f43aadc1fdb3f8918da4aa51acaeae59cc380
柚子 <106157421@qq.com>
2020-05-03 16:55:44 +0800

    商品列表截取字符串

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -9,6 +9,7 @@ use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
+use Illuminate\Support\Str;
 
 class ProductController extends AdminController
 {
